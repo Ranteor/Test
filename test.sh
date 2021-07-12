210a748837fba7316f564bfdd7206de5c055da66
Ranteor <drake59@bk.ru>
2021-07-12 12:02:26 +0500

    main

--- test.sh
+++ test.sh
@@ -1,2 +1,13 @@
 #! /bin/bash
-echo "test"
+set -eou pipefail
+while true
+do
+curl -w  "dnslookup: %{time_namelookup} | "`
+         `"Connect: %{time_connect} | "`
+         `"Appconnect: %{time_appconnect} | "`
+         `"Pretransfer: %{time_pretransfer} | "`
+         `"TTFB: %{time_starttransfer} | "`
+         `"Total time: %{time_total} | "`
+         `"Size: %{size_download}\n" https://00.img.avito.st/image/1/1cs1qbaxeSIDHvsvE_qxrtcKeSaJCMg
+sleep 1
+done
