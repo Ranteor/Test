25b3135de9f4335817374e7025447e77eaac0a00
Ranteor <drake59@bk.ru>
2021-07-12 14:59:28 +0500

    main

--- test2.sh
+++ test2.sh
@@ -1,1 +1,17 @@
 #!/bin/bash
+# case перменная in
+#    условие)
+#    команда 1;;
+#    команда 2;;
+# esac
+echo "Введите ваш возраст"
+read age
+if [[ $age -ge 0 ]] && [[ $age -lt 12 ]] ; then
+    echo "Вы ещё ребенок"
+    elif [[ $age -ge 12 ]] && [[ $age -lt 18 ]]; then
+    echo "Вы подросток"
+    elif [[ $age -ge 18 ]] && [[ $age -lt 60 ]]; then
+    echo "Вы уже взрослый"
+    else
+    echo "Вы старый" 
+fi 
