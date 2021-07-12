05e54e174ac508d70bb9c6b0cb5f18bcdd91ec07
Ranteor <drake59@bk.ru>
2021-07-12 12:31:39 +0500

    main

--- test.sh
+++ test.sh
@@ -4,6 +4,13 @@ while true
 do
 echo "======="
 echo "Connect to:https://00.img.avito.st/image/1/1cs1qbaxeSIDHvsvE_qxrtcKeSaJCMg"
+curl -w  "dnslookup: %{time_namelookup} | "`
+         `"Connect: %{time_connect} | "`
+         `"Appconnect: %{time_appconnect} | "`
+         `"Pretransfer: %{time_pretransfer} | "`
+         `"TTFB: %{time_starttransfer} | "`
+         `"Total time: %{time_total} | "`
+         `"Size: %{size_download}\n"  https://00.img.avito.st/image/1/1cs1qbaxeSIDHvsvE_qxrtcKeSaJCMg
 echo "======="
 echo "Connect to:https://68.img.avito.st/image/1/rS2IB7axAcS-sIPJ3gH0BXGkAcA0pgvG"
 curl -w  "dnslookup: %{time_namelookup} | "`
@@ -12,6 +19,6 @@ curl -w  "dnslookup: %{time_namelookup} | "`
          `"Pretransfer: %{time_pretransfer} | "`
          `"TTFB: %{time_starttransfer} | "`
          `"Total time: %{time_total} | "`
-         `"Size: %{size_download}\n"  https://00.img.avito.st/image/1/1cs1qbaxeSIDHvsvE_qxrtcKeSaJCMg  ||  https://68.img.avito.st/image/1/rS2IB7axAcS-sIPJ3gH0BXGkAcA0pgvG
-sleep 10
+         `"Size: %{size_download}\n" https://68.img.avito.st/image/1/rS2IB7axAcS-sIPJ3gH0BXGkAcA0pgvG
+sleep 1
 done
