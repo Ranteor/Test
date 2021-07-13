f63886ee364b9f0bbfcffd7320194e33536845f8
Ranteor <drake59@bk.ru>
2021-07-13 12:20:56 +0500

    main

--- test2.sh
+++ test2.sh
@@ -4,14 +4,20 @@
 #    команда 1;;
 #    команда 2;;
 # esac
-echo "Введите ваш возраст"
-read age
-if [[ $age -ge 0 ]] && [[ $age -lt 12 ]] ; then
-    echo "Вы ещё ребенок"
-    elif [[ $age -ge 12 ]] && [[ $age -lt 18 ]]; then
-    echo "Вы подросток"
-    elif [[ $age -ge 18 ]] && [[ $age -lt 60 ]]; then
-    echo "Вы уже взрослый"
-    else
-    echo "Вы старый" 
-fi
+# echo "Введите ваш возраст"
+# read age
+# if [[ $age -ge 0 ]] && [[ $age -lt 12 ]] ; then
+#     echo "Вы ещё ребенок"
+#     elif [[ $age -ge 12 ]] && [[ $age -lt 18 ]]; then
+#     echo "Вы подросток"
+#     elif [[ $age -ge 18 ]] && [[ $age -lt 60 ]]; then
+#     echo "Вы уже взрослый"
+#     else
+#     echo "Вы старый" 
+# fi
+# read x
+# read y
+# echo "URL1:$x"
+# echo "URL2:$y
+x=$date
+echo "$x"
