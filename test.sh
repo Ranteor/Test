341cfcc46fa491fbe39998711e9de41aaba4f87e
Ranteor <drake59@bk.ru>
2021-07-16 14:22:08 +0500

    main

--- test.sh
+++ test.sh
@@ -1,10 +1,13 @@
 #! /bin/bash
 set -eou pipefail
-for (( i=0; i<=7000; i++ )); do
-read z
-read n
-echo "URL 1: $z"
-echo "URL 2: $n"
+echo "Enter the number of request:"
+read x
+while [ $x -gt 0 ]; do
+for (( $x=2; $x<=$x $x++ ))
+do
+if [ $x]
+echo "URL 1: https://00.img.avito.st/image/1/1cs1qbaxeSIDHvsvE_qxrtcKeSaJCM "
+echo "URL 2: https://68.img.avito.st/image/1/rS2IB7axAcS-sIPJ3gH0BXGkAcA0pgvG "
 X=(curl -w %{time_namelookup} %{time_connect} %{time_appconnect} %{time_pretransfer} %{time_starttransfer} -H Connection: close -o /home/pnc -s -v https://00.img.avito.st/image/1/1cs1qbaxeSIDHvsvE_qxrtcKeSaJCM)
 Y=(curl -w %{time_namelookup} %{time_connect} %{time_appconnect} %{time_pretransfer} %{time_starttransfer} -H Connection: close -o /home/pnc -s -v https://68.img.avito.st/image/1/rS2IB7axAcS-sIPJ3gH0BXGkAcA0pgvG)
 echo ${X[@]}
