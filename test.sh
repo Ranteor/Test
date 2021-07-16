2911d62f350a122a7826982b61c6ad182750a34f
Ranteor <drake59@bk.ru>
2021-07-16 14:58:31 +0500

    main

--- test.sh
+++ test.sh
@@ -2,10 +2,11 @@
 set -eou pipefail
 echo "Enter the number of request:"
 read x
-while [ $x -gt 0 ]; do
-for (( $x=2; $x<=$x $x++ ))
+for var1 in $x
 do
-if [ $x]
+if [ $var1 -gt 0 ]; then
+break
+fi
 echo "URL 1: https://00.img.avito.st/image/1/1cs1qbaxeSIDHvsvE_qxrtcKeSaJCM "
 echo "URL 2: https://68.img.avito.st/image/1/rS2IB7axAcS-sIPJ3gH0BXGkAcA0pgvG "
 X=(curl -w %{time_namelookup} %{time_connect} %{time_appconnect} %{time_pretransfer} %{time_starttransfer} -H Connection: close -o /home/pnc -s -v https://00.img.avito.st/image/1/1cs1qbaxeSIDHvsvE_qxrtcKeSaJCM)
